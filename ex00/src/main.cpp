fcf85d55fa921e1d9e95542fdf12c0bf477fa01a
ghambrecht <gabriel.hambrecht@gmail.com>
2026-01-15 10:37:02 +0100

    first make

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,9 +6,16 @@
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 17:33:41 by ghambrec          #+#    #+#             */
-/*   Updated: 2026/01/14 17:35:01 by ghambrec         ###   ########.fr       */
+/*   Updated: 2026/01/15 10:36:17 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Bureaucrat.hpp"
 
+int main(void)
+{
+	Bureaucrat a("tim", 3);
+	Bureaucrat b("nick", 149);
+	Bureaucrat c("paul", 1);
+	Bureaucrat d("bernd", 156);
+}
